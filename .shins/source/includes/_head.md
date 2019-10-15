3cffe9dae545135cfe4905ef01cd1a9d683071af
Jeff Shillitto <jeff@shillitto.com>
2019-10-15 19:54:44 +1100

    Integrate Google Analytics tracking

--- .shins/source/includes/_head.md
+++ .shins/source/includes/_head.md
@@ -1,2 +1,3 @@
 <!-- shins; content for the head element from source/includes/_head.md -->
 <meta name="theme-color" content="#F3F7F9">
+{{GA}}
--- build-docs.sh
+++ build-docs.sh
@@ -1,6 +1,7 @@
 DOCS_DIR=build/docs
 mkdir -p $DOCS_DIR
 
+# Convert OpenAPI to doc to Shins Markdown
 ./node_modules/.bin/widdershins \
     --theme dracula \
     --language_tabs shell:Curl http:HTTP javascript--nodejs:NodeJS javascript:jQuery php:PHP ruby:Ruby python:Python java:Java go:Go \
@@ -9,6 +10,7 @@ mkdir -p $DOCS_DIR
 
 cp $DOCS_DIR/index.html.md .shins/source/index.html.md
 
+# Build the Shins docs HTML
 cd .shins
 node shins.js \
     --logo ../assets/img/logo.png \
@@ -23,5 +25,9 @@ cp -r .shins/pub ./$DOCS_DIR/.
 cp -r .shins/source/images/custom_logo.png ./$DOCS_DIR/source/images/custom_logo.png
 cp -r .shins/source/images/navbar.png ./$DOCS_DIR/source/images/navbar.png
 cp -r .shins/source/fonts ./$DOCS_DIR/source/fonts
+
+# Insert Google Analytics
+sed -e "/{{GA}}/{r .ga" -e "d}" ./$DOCS_DIR/index.html > ./$DOCS_DIR/index.tmp.html
+mv -f ./$DOCS_DIR/index.tmp.html ./$DOCS_DIR/index.html
 
 rm ./$DOCS_DIR/index.html.md
