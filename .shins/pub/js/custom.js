9a3267e54dd138af6abbb5c50d76b3d2c6ec3833
Jeff Shillitto <jeff@shotstack.io>
2024-07-25 13:21:23 +1000

    Add schemas and layout updates for tweening and keyframes

--- .shins/pub/js/custom.js
+++ .shins/pub/js/custom.js
@@ -1,11 +1,48 @@
-function hideDiscriminatorTables() {
-    var $discriminatorTable = $('em:contains("anonymous")').closest('table');
+function hideDiscriminatorColumn() {
+    $('table em:contains("anonymous")').each(function() {
+        var $em = $(this);
+        var $td = $em.closest('td');
+        var colIndex = $td.index();
+        $td.closest('table').find('tr').each(function() {
+            $(this).find('td:eq(' + colIndex + ')').remove();
+            $(this).find('th:eq(' + colIndex + ')').remove();
+        });
+    });
+}
 
-    $discriminatorTable.prevUntil('#properties').remove();
-    $discriminatorTable.prev().remove();
-    $discriminatorTable.remove();
+function updateOneOfText() {
+    $('p:contains("oneOf")').each(function() {
+        var updatedText = $(this).text().replace(/oneOf/g, 'Value must be one of the following:');
+        $(this).text(updatedText);
+    });
+}
+
+function updateAnyOfText() {
+    $('p:contains("anyOf")').each(function() {
+        var updatedText = $(this).text().replace(/anyOf/g, 'Value can be any combination of the following:');
+        $(this).text(updatedText);
+    });
+}
+
+function replaceXorWithOr() {
+    $('p:contains("xor")').each(function() {
+        var updatedText = $(this).text().replace(/xor/gi, '- or -');
+        $(this).text(updatedText);
+    });
+}
+
+function removeContinuedTag() {
+    $('p').each(function() {
+        if ($.trim($(this).text()) === 'continued') {
+            $(this).remove();
+        }
+    });
 }
 
 $(document).ready(function() {
-    hideDiscriminatorTables();
+    updateOneOfText();
+    updateAnyOfText();
+    hideDiscriminatorColumn();
+    replaceXorWithOr();
+    removeContinuedTag();
 });
